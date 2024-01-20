e535d137154e902e0f81c696e68592a3f942408e
cartran <cartran@uiowa.edu>
2024-01-19 22:08:17 -0600

    Changing navbar, adding headshot

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,9 +1,11 @@
+import headshot from './assets/headshot.jpg'
+
 
 const Home = () => {
     return (
         // <div class="flex border-b">
         //     <div class="w-1/3 border-r p-4">
-        //         <h1 class="text-4xl">Carlo Tran</h1>
+        //         <h1 class="text-4xl ">Carlo Tran</h1>
         //         <p>Computer Science B.S. at University of Iowa, 2026</p>
         //         <p>Current Intern with the Driving Safety Research Institute and the Computational Epidemiology Research Group</p>
         //         <p>Future Intern at Casey's General Stores</p>
@@ -14,7 +16,17 @@ const Home = () => {
         //         <p>My Skillset: Node.Js, React.Js, Tailwind CSS, C++, Java, Python, SQL, R</p>
         //     </div>
         // </div>
-        <></>
+        <>
+        <div class="flex">
+            <div class="w-2/3 p-4">
+                <p class="text-5xl">I am a Computer Science Student with a passion for learning, and years of relevant work experience</p>
+                <p class="text-2xl py-4">I hope to make the world a better place, by writing clean and elegant code.</p>
+            </div>
+        </div>
+        <div class="flex">
+            <img class="w-1/3" src={headshot} alt="Carlo Tran's Headshot"/>
+        </div>
+        </>
     )
 }
 
--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Outlet, Link } from "react-router-dom";
+import resume from './assets/Carlo Tran- Resume.pdf';
 
 const NavBar = () => {
   return (
@@ -6,7 +7,7 @@ const NavBar = () => {
       <nav class="flex items-center justify-between flex-wrap p-5 bg-neutral-800 shadow-xl">
         <div class="w-full block flex-grow">
           <Link class="block mt-4 lg:inline-block lg:mt-0 text-neutral-300 hover:text-white mr-4" to="/">Home</Link>
-          <Link class="block mt-4 lg:inline-block lg:mt-0 text-neutral-300 hover:text-white mr-4" to="/Resume">Resume</Link>
+          <Link class="block mt-4 lg:inline-block lg:mt-0 text-neutral-300 hover:text-white mr-4" target='_blank' to={resume}>Resume</Link>
           <Link class="block mt-4 lg:inline-block lg:mt-0 text-neutral-300 hover:text-white mr-4" target='_blank' to="https://github.com/carlotran4">GitHub</Link>
           <Link class="block mt-4 lg:inline-block lg:mt-0 text-neutral-300 hover:text-white mr-4" target='_blank' to="https://linkedin.com/in/carlo-tran/">LinkedIn</Link>
         </div>
