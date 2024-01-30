99e85792600a5d5710f60396adb00b661307eae2
cartran <cartran@uiowa.edu>
2024-01-29 22:08:29 -0600

    editing to work with firebase

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,9 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
 const path = require("path");
